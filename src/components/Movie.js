958267a72173d2b22f7a6070b5842a3bac8ea6fc
Teddy <note@kakao.com>
2021-03-11 21:10:10 +0900

    [210311] done

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,24 +5,24 @@ import "./Movie.css";
 
 function Movie({ id, year, title, summary, poster, genres }) {
   return (
-    <Link
-      to={{
-        pathname: `/movie/${id}`,
-        state: {
-          year,
-          title,
-          summary,
-          poster,
-          genres,
-        },
-      }}
-    >
-      <div className="movie">
+    <div className="movie">
+      <Link
+        to={{
+          pathname: `/movie/${id}`,
+          state: {
+            year,
+            title,
+            summary,
+            poster,
+            genres,
+          },
+        }}
+      >
         <img src={poster} alt={title} title={title} />
         <div className="movie__data">
           <h3 className="movie__title">{title}</h3>
           <h5 className="movie__year">{year}</h5>
-          <ul className="genres">
+          <ul className="movie__genres">
             {genres.map((genre, index) => (
               <li key={index} className="genres__genre">
                 {genre}
@@ -31,19 +31,18 @@ function Movie({ id, year, title, summary, poster, genres }) {
           </ul>
           <p className="movie__summary">{summary.slice(0, 180)}...</p>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
 
-Movie.propType = {
+Movie.propTypes = {
   id: PropTypes.number.isRequired,
   year: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
-  // rating: PropTypes.number.isRequired,
 };
 
 export default Movie;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -31,19 +31,17 @@ class Home extends React.Component {
           </div>
         ) : (
           <div className="movies">
-            {movies.map((movie) => {
-              return (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                />
-              );
-            })}
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
           </div>
         )}
       </section>
